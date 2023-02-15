519b3b76e7bb95660d92277189ec660d61be9968
Jasmin2410 <108186055+Jasmin2410@users.noreply.github.com>
2023-02-15 15:08:40 +0530

    nav_bar1

--- a.js
+++ a.js
@@ -2,3 +2,19 @@ function myfunction(){
     document.getElementById("mytopnav").classList.toggle("responsive");
     
 }
+//navbar sticky
+window.onscroll = function() {myFunction()};
+
+
+var mytopnav = document.getElementById("mytopnav");
+
+var sticky = mytopnav.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    mytopnav.classList.add("sticky")
+  } else {
+    mytopnav.classList.remove("sticky");
+  }
+}
+// -----------------------------------------------------------------------------------------------------
