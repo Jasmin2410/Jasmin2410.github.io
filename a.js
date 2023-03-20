5d0dda1cb74d8cd23bd0a97854b9f02a1e6bbc84
Jasmin2410 <108186055+Jasmin2410@users.noreply.github.com>
2023-03-20 20:22:21 +0530

    styling4

--- a.js
+++ a.js
@@ -19,12 +19,12 @@ function myFunction() {
 }
 // -----------------------------------------------------------------------------------------------------
 
-document.getElementById('resume-link-1').addEventListener("click",()=>{
-  window.location.assign("https://drive.google.com/file/d/1UPjs4mElLoNl869vxKXq_YCSisOWX8vk/view?usp=sharing", "_blank");
-})
-document.getElementById('resume-link-2').addEventListener("click",()=>{
-  window.location.assign("https://drive.google.com/file/d/1UPjs4mElLoNl869vxKXq_YCSisOWX8vk/view?usp=sharing", "_blank");
-})
+// document.getElementById('resume-link-1').addEventListener("click",()=>{
+//   window.location.assign("https://drive.google.com/file/d/1UPjs4mElLoNl869vxKXq_YCSisOWX8vk/view?usp=sharing", "_blank");
+// })
+// document.getElementById('resume-link-2').addEventListener("click",()=>{
+//   window.location.assign("https://drive.google.com/file/d/1UPjs4mElLoNl869vxKXq_YCSisOWX8vk/view?usp=sharing", "_blank");
+// })
 
 // document.getElementById('resume-link-1').addEventListener("click",()=>{
 //   window.location.assign("https://drive.google.com/file/d/192ndBi1PBzRkMeaumqFobqZ1BF-GIhAM/view?usp=share_link", "_blank");
